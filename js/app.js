8e12c7cad65ed3aefb8b6dbea7ded153d91a566f
Dustin Keppler <dtkeppler@yahoo.com>
2015-03-19 19:26:44 -0400

    added .remove-button alert

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 
 $( document ).ready(function () {
 	$( "#add-button" ).click(function() {
-	$( "#list" ).append("<li class='list-item'><button class='remove'>-</button> " + $("#add-item").val() + "</li>");
+		$( "#list" ).append("<li class='list-item'><button class='remove-button'>-</button> " + $("#add-item").val() + "</li>");
+	})
+	$(".remove-button" ).click(function() {
+		alert("The remove button has been clicked");
 	})
 });
