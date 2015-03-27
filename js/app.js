4c697950b78035634c453264e9895f3b83626f99
Dustin Keppler <dtkeppler@yahoo.com>
2015-03-26 20:51:45 -0400

    fixed remove-button issues

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
-
 $( document ).ready(function () {
 	$( "#add-button" ).click(function() {
-		$( "#list" ).append("<li class='list-item'><button class='remove-button'>-</button> " + $("#add-item").val() + "</li>");
+		if(!$( "#add-item" ).val()) {
+			alert("Please enter an item into the list!");
+		}
+		else {$( "#list" ).append("<li class='list-item'><input type='checkbox'><span> " + $("#add-item").val() + "</span><button class='remove-button'>-</button> </li>");
+		$( "#add-item" ).val('');
+		}
 	})
-	$(".remove-button" ).click(function() {
-		alert("The remove button has been clicked");
+
+
+	$( "input[type='checkbox']").click(function() {
+   		if (this.checked) {
+        	//alert($(this).next().attr('class'));
+        	$(this).next().addClass("strikethrough");
+    	} 
+    	else {
+        	//alert('Unchecked');
+        	$(this).nextSibling.removeClass("strikethrough");
+    	}
 	})
+
+
+});
+
+$(document).on('click', ".remove-button", function () {
+    //alert($(this).parent().attr('class'));
+		$(this).parent().remove();
+    return false;
 });
